167685c635ff8ff820e44fb9489f81d3df15e65d
daniel.victor9@hotmail.com <daniel.victor9@hotmail.com>
2019-05-27 00:36:37 -0300

    add files

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+const authRoute = require('./routes/auth');
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_CONNECT, { useNewUrlParser: true }, () => 
+    console.log('Connected to DB')
+);
+
+app.use(express.json());
+
+app.use('/api/user', authRoute);
+
+app.listen(3000, () => console.log('Server is Running...'));
